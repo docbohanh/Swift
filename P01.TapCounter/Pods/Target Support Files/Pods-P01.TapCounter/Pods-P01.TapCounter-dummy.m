692226d8858a36735d291501cb547140fe012953
La Huu Thanh <huuthanhla@gmail.com>
2016-12-28 23:33:22 +0700

    Add SnapKit via cocoa pod

---
+++ P01.TapCounter/Pods/Target Support Files/Pods-P01.TapCounter/Pods-P01.TapCounter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_P01_TapCounter : NSObject
+@end
+@implementation PodsDummy_Pods_P01_TapCounter
+@end
---
+++ P01.TapCounter/Pods/Target Support Files/Pods-P01.TapCounter/Pods-P01.TapCounter-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_P01_TapCounterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_P01_TapCounterVersionString[];
+
