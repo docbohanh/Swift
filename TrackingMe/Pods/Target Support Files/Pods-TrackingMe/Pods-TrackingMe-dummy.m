b552f24c235bb1392d0d9c886882bffba81544d1
La Huu Thanh <taffy_nikolas@yahoo.com>
2017-01-13 07:59:15 +0700

    Init project `TrackingMe` and pod GoogleMap

---
+++ TrackingMe/Pods/Target Support Files/Pods-TrackingMe/Pods-TrackingMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TrackingMe : NSObject
+@end
+@implementation PodsDummy_Pods_TrackingMe
+@end
---
+++ TrackingMe/Pods/Target Support Files/Pods-TrackingMeTests/Pods-TrackingMeTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TrackingMeTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TrackingMeTestsVersionString[];
+
